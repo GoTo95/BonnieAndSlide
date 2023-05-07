18403863a3fcb99e3c7c7a5ae52e99469c4987ba
GoTo95 <uraprudnikov@yandex.ru>
2023-05-07 15:18:30 +0300

    Initial

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
 setup(
-    # Whatever arguments you need/want
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Bonnieandslide',
+    url='https://github.com/GoTo95/BonnieAndSlide',
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT'
 )
