25144e01fcc73b4eb9d846c3f50c61f3258c0fa4
GoTo95 <uraprudnikov@yandex.ru>
2023-05-07 14:51:39 +0300

    Initial

--- libs/test.py
+++ libs/test.py
@@ -2,11 +2,12 @@ import requests
 from io import BytesIO
 
 # URL файла на GitHub
-file_url = 'https://raw.githubusercontent.com/user/repo/branch/path/to/file.py'
+file_url = 'https://rawgithub.com/GoTo95/BonnieAndSlide/blob/master/main.py'
 
 # загрузка содержимого файла
 response = requests.get(file_url)
 content = BytesIO(response.content)
-
-# выполнение кода файла
-exec(content.read())
+print(response.content)
+# print(content.read())
+# # выполнение кода файла
+# exec(content.read())
