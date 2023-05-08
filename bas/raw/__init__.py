d9c422a4dd55f10eaf939733e2070aa41a752f6b
GoTo95 <uraprudnikov@yandex.ru>
2023-05-08 15:21:41 +0300

    Final

--- bas/raw/__init__.py
+++ bas/raw/__init__.py
@@ -1,1 +1,2 @@
 from bas.raw.amo_lead import get_leads
+from bas.raw.amo_companies import get_companies
---
+++ bas/raw/amo_companies.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime
+from clickhouse_driver import Client
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+def get_leads(row):
+    return [el['id'] for el in row['leads']]
+
+
+def get_contacts(row):
+    return [el['id'] for el in row['contacts']]
+
+
+def get_companies(date_from, token):
+    _url = 'https://testbonnieandslide.amocrm.ru/api/v4/companies'
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+
+    last_created_at = datetime.strptime(date_from, '%Y-%m-%dT%H:%M:%S')
+    params = {
+        'page': 1,
+        "filter[updated_at][from]": int(last_created_at.timestamp()),
+        "filter[updated_at][to]": int(datetime.now().timestamp()),
+        "limit": 250,
+        'with': 'leads,contacts'
+    }
+
+    df = pd.DataFrame()
+
+    while True:
+        resp = requests.get(_url, params=params, headers=headers)
+        status = resp.status_code
+        if status == 200:
+            r = resp.json()
+            chunk = pd.DataFrame(r['_embedded']['companies'])
+            chunk = chunk.replace({np.nan: None})
+            chunk['created_at'] = chunk['created_at'].apply(datetime.fromtimestamp)
+            chunk['updated_at'] = chunk['updated_at'].apply(datetime.fromtimestamp)
+            df = df.append(chunk)
+
+            params['page'] += 1
+            if params['page'] % 100 == 0:
+                print(params['page'], chunk['updated_at'].min())
+        else:
+            print(f'http status code is {status}')
+            break
+
+    df['lead_id'] = df['_embedded'].apply(
+        lambda row: get_leads(row)
+    )
+
+    df['contact_id'] = df['_embedded'].apply(
+        lambda row: get_contacts(row)
+    )
+
+    needed_columns = ['id', 'created_at', 'updated_at', 'responsible_user_id', 'name', 'lead_id', 'contact_id']
+    df = df[needed_columns]
+
+    df = df.explode('lead_id')
+    df = df.explode('contact_id')
+
+    return df
