e7895a156aca795137fb7ff2a6189f9fc12e5499
Jurijj161 <yuriy.scherbatov@skyeng.ru>
2023-09-28 23:15:48 +0300

    fix and upd amo leads

--- bas/raw/amo_lead.py
+++ bas/raw/amo_lead.py
@@ -8,7 +8,8 @@ from urllib import parse
 def get_field_val(row, field_name):
     if type(row) == list:
         for field in row:
-            if field['field_name'] == field_name:
+            # if field['field_name'] == field_name:
+            if field['field_id'] == field_name:
                 return field['values'][0]['value']
 
 
@@ -69,18 +70,33 @@ def get_leads(date_from, token):
             r = resp.json()
             chunk = pd.DataFrame(r['_embedded']['leads'])
 
+            # fields = {
+            #     'referrer': 'referrer',
+            #     'getcourse_deal_id': 'GetCourseId',
+            #     'getcourse_deal_number': 'GetCourse deal_number',
+            #     'assumed_payment_date': 'Дата оплаты',
+            #     'gclientid': 'gclientid',
+            #     'web_id': 'web_id',
+            #     'course': 'Курс',
+            #     'utm_source': 'utm_source',
+            #     'utm_medium': 'utm_medium',
+            #     'utm_campaign': 'utm_campaign',
+            #     'utm_content': 'utm_content'
+            # }
+
             fields = {
-                'referrer': 'referrer',
-                'getcourse_deal_id': 'GetCourseId',
-                'getcourse_deal_number': 'GetCourse deal_number',
-                'assumed_payment_date': 'Дата оплаты',
-                'gclientid': 'gclientid',
-                'web_id': 'web_id',
-                'course': 'Курс',
-                'utm_source': 'utm_source',
-                'utm_medium': 'utm_medium',
-                'utm_campaign': 'utm_campaign',
-                'utm_content': 'utm_content'
+                'referrer': 1143965, #'referrer',
+                'getcourse_deal_id': 1052443, #'GetCourseId',
+                'getcourse_deal_number': 1163653, #'GetCourse deal_number',
+                'assumed_payment_date': 853013, #'Дата оплаты',
+                'gclientid': 851993, #'gclientid',
+                'web_id': 1167847, #'web_id',
+                'course': 1103023, #'Курс',
+                'utm_source': 851973, #'utm_source',
+                'utm_medium': 851969, #'utm_medium',
+                'utm_campaign': 851971, # 'utm_campaign',
+                'utm_content': 851967, #'utm_content'
+                'course_by_closed_deal': 1165007  # 'Курс при закрытии сделки'
             }
 
             for key, value in fields.items():
@@ -132,7 +148,7 @@ def get_leads(date_from, token):
                           'getcourse_deal_id', 'getcourse_deal_number', 'lost_reason', 'contact_id', 'referrer',
                           'responsible_user_id', 'closed_at', 'closest_task_at',
                           'assumed_payment_date', 'course', 'tags', 'utm_source', 'utm_medium', 'utm_campaign',
-                          'utm_content']
+                          'utm_content', 'course_by_closed_deal']
 
         df = df[needed_columns]
 
