7d5833c345fb55f8b25b556d6a5e378bf037308b
Jurijj161 <yuriy.scherbatov@skyeng.ru>
2023-10-23 08:37:40 +0300

    try1

--- bas/raw/amo_contacts.py
+++ bas/raw/amo_contacts.py
@@ -60,9 +60,9 @@ def get_contacts(date_from, token):
         'utm_source': 1162663,
         'utm_medium': 1162665,
         'utm_term': 1162669,
-        'utm_content': 1162671,
-        'email': 851961,
-        'phone': 851959
+        'utm_content': 1162671
+        # 'email': 851961,
+        # 'phone': 851959
             }
 
     for field in fields.keys():
@@ -73,6 +73,11 @@ def get_contacts(date_from, token):
     df['webinars'] = df['custom_fields_values'].apply(
         lambda row, field_name='Вебинар': get_webinar(row, field_name)
     )
+
+    #test
+    df['email'] = ''
+    df['phone'] = ''
+    #end
 
     needed_columns = ['id', 'created_at', 'updated_at', 'responsible_user_id', 'utm_campaign', 'utm_source',
                       'utm_medium', 'utm_term', 'utm_content', 'webinars', 'email', 'phone']
