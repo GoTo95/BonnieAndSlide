1e420d604aa7e6ebf743daee98ec7c5ccffc8beb
Jurijj161 <yuriy.scherbatov@skyeng.ru>
2024-03-30 18:46:42 +0300

    created_ad to status_changed_at - v2

--- bas/raw/orders.py
+++ bas/raw/orders.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 
 def get_orders(date_from, token):
-    date_from_with_lag = datetime.strptime(date_from, '%Y-%m-%dT%H:%M:%S') - timedelta(31) #попробуем забирать каждый раз за последние 31 день, т.к. в ГК задним числом что-то появляется
+    date_from_with_lag = datetime.strptime(date_from, '%Y-%m-%dT%H:%M:%S') - timedelta(3)
     key = 'Jm8OSMY9ju2UtnBW6CHHYvRu8CdGNo4cgmBtNO2skBjyMDaSgF5WoXwlvERhjmms37d1UZ9YY3U7vhmnHvdrbyRXsx7KSxefL6AfNYIHguw4HXppIsfMcrYqvpW0p6gt'
     r_text = 'https://bonnieandslide.getcourse.ru/pl/api/account/deals?key={}&status_changed_at[from]={}'.format(key,
                                                                                                           date_from_with_lag) #date_from)
@@ -31,9 +31,6 @@ def get_orders(date_from, token):
     df.columns = ['id', 'number', 'user_id', 'created_at', 'updated_at', 'tag']
     df['tag'] = df['tag'].apply(lambda x: str(x))
 
-    #иногда заказ создается словно задним числом в ГК (и это действо создания в ГК происходит, видимо, в updated_at),
-    # поэтому created_at не всегда подходит, и т.к. updated_at не всегда заполнен, будем брать поочередно
-    # По сути, все ради инкремента
     df.loc[(df['updated_at'] == '') | (df['updated_at'].isna()), 'updated_at'] = df['created_at']
 
     df[['id', 'number', 'user_id']] = df[['id', 'number', 'user_id']].replace('', -1).astype(int)
